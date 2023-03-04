b8c07b67ae8af997d7ebe60b29720f5f04e3db18
josejmv <josejmvasquez@gmail.com>
2023-03-03 23:34:57 -0400

    [hotfix] setup project as a pwa

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,24 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
+  const keywords = [
+    'todo',
+    'list',
+    'josejmv',
+    'next.js',
+    'cypress',
+    'project',
+    'react.js'
+  ]
+
   return (
     <Html lang='en'>
       <Head>
+        <meta charSet='utf-8' />
         <link rel='icon' href='/favicon.ico' />
+        <meta name='author' content='JoseJMV' />
+        <meta name='copyright' content='JoseJMV' />
+        <meta name='keywords' content={keywords.join()} />
       </Head>
 
       <body>
